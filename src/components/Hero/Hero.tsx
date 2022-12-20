ef11c6d273cc50d08eb8270c362f9848aefe650f
Emanuel Lorenzo <aemalorenzo@gmail.com>
2022-12-20 04:00:10 -0300

    no longer needed message, its cool enough

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,9 +4,6 @@ import Link from 'next/link';
 export const Hero = () => {
   return (
     <section className="h-full flex flex-col justify-center items-center">
-      <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        This will be a cool showcase of my work. Someday.
-      </motion.p>
       <Link passHref href="/blog">
         <a className="mt-4">Go to blog</a>
       </Link>
